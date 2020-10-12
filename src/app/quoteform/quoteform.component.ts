703ea09c2469cc9265c4a21f176f752e92acf00f
Billy Chepkemoi <cnafayo@gmail.com>
2020-10-12 19:37:22 +0300

    updating form component ts

--- src/app/quoteform/quoteform.component.ts
+++ src/app/quoteform/quoteform.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { QuoteB } from '../quote-b';
 @Component({
-  selector: 'app-quoteform',
-  templateUrl: './quoteform.component.html',
-  styleUrls: ['./quoteform.component.css']
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
 })
-export class QuoteformComponent implements OnInit {
+export class QuoteFormComponent implements OnInit {
+
+  newQuote = new QuoteB(0, "", "", "", new Date(), 0, 0)
+  @Output() sendQuote = new EventEmitter<QuoteB>();
+  @Input() hideForm: boolean;
+  addQuote() {
+    this.sendQuote.emit(this.newQuote);
+    this.newQuote = new QuoteB(0, " ", " ", " ", new Date(), 0, 0)
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
