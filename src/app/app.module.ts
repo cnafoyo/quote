a81b8542fab9e3e57ee8adbe8d8697dfd313a14c
Billy Chepkemoi <cnafayo@gmail.com>
2020-10-12 19:49:40 +0300

    modifying module ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ThequoteComponent } from './thequote/thequote.component';
-import { QuoteformComponent } from './quoteform/quoteform.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteNavComponent } from './quote-nav/quote-nav.component';
+import { QuoteDatePipe } from './quote-date.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ThequoteComponent,
-    QuoteformComponent
+    QuoteComponent,
+    QuoteDetailsComponent,
+    QuoteFormComponent,
+    QuoteNavComponent,
+    QuoteDatePipe,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
